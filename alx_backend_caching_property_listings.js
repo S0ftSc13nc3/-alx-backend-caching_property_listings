ac46f5ea04e1f9c2472efc714841c6646478de50
Kopano Leshaba <138041617+S0ftSc13nc3@users.noreply.github.com>
2025-08-31 23:09:35 +0200

    Add files via upload

---
+++ alx_backend_caching_property_listings.js
@@ -0,0 +1,10 @@
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'property_db',
+        'USER': 'property_user',
+        'PASSWORD': 'property_pass',
+        'HOST': 'postgres',  # Service name in docker-compose.yml
+        'PORT': '5432',
+    }
+}
